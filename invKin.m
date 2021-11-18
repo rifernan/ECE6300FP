28ab8dedbf4758705271f45096ba5a9ea4176aaa
rifernan <90804291+rifernan@users.noreply.github.com>
2021-11-18 13:03:41 -0600

    Update invKin.m
    Updated line 12 to give correct value for theta 1

--- invKin.m
+++ invKin.m
@@ -9,7 +9,7 @@ for k = 1:length(BTheta)
    
    D= (x^2+y^2-L1^2-L2^2)/(2*L1*L2);
    Theta2=atan2(sqrt(1-D^2),D);
-   Theta1=atan2(y,x)-atan2(L2*sin(Theta2),L1+L2*cos(Theta2));
+   Theta1=((atan2(y,x)-atan2(L2*sin(Theta2),L1+L2*cos(Theta2)))*-1)+(pi/2);
     b(j,1)=Theta1;
     b(j,2)=Theta2;
     
